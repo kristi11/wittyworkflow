2b686f898a2a021eb2935660bb6fd24beeb05257
kristi <tanellari@gmail.com>
2024-01-10 01:20:45 -0500

    wip

--- database/factories/BusinessHourFactory.php
+++ database/factories/BusinessHourFactory.php
@@ -13,11 +13,11 @@ class BusinessHourFactory extends Factory
     public function definition(): array
     {
         return [
+            'user_id' => '1',
             'day' => $this->faker->unique()->dayOfWeek(),
             'open_from' => $this->faker->time($format = 'h:i A'),
             'open_until' => $this->faker->time($format = 'h:i A', $max = '12:00 PM'),
             'open' => $this->faker->boolean(),
-            'phone' => $this->faker->phoneNumber(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
