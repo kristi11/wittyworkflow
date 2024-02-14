0ffa35e4c4c4d4757f43de2e192ddb861d89a901
kristi <tanellari@gmail.com>
2024-02-14 00:50:40 -0500

    Change admin services layout

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,9 @@ export default defineConfig({
                 ...refreshPaths,
                 'app/Livewire/**',
             ],
-            external: ['@alpinejs/ui/dist/cdn.js'],
+            rollupOptions: {
+                external: ['@alpinejs/ui/dist/cdn.js'],
+            },
         }),
     ],
 });
