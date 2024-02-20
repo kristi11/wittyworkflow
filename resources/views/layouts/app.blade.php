c7a8d49d7b0e23e602abd6782518c336e7920907
kristi <tanellari@gmail.com>
2024-02-20 13:38:12 -0500

    edit manifest.json

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
+    <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Styles -->
