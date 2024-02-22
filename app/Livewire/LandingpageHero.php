f6cfb91241ac399be090e386d97990a59f890a3f
kristi <tanellari@gmail.com>
2024-02-21 21:14:06 -0500

    Make sensitive functions private and refactor for beter readability

--- app/Livewire/LandingpageHero.php
+++ app/Livewire/LandingpageHero.php
@@ -15,21 +15,26 @@ use Livewire\WithFileUploads;
 class LandingpageHero extends Component
 {
     use withFileUploads;
-    public $hero;
-    public $mainQuote;
-    public $secondaryQuote;
-    public $thirdQuote;
-    public $gradientDegree;
-    public $gradientFirstColor;
-    public $gradientDegreeStart;
-    public $gradientSecondColor;
-    public $gradientDegreeEnd;
-    public $waves;
-        /**
-         * @var true
-         */
-    public bool $showHeroModal;
-    public $image;
+
+public $hero;
+
+public $mainQuote;
+public $secondaryQuote;
+public $thirdQuote;
+public $gradientDegree;
+public $gradientFirstColor;
+public $gradientDegreeStart;
+public $gradientSecondColor;
+public $gradientDegreeEnd;
+public $waves;
+    /**
+     * @var true
+     */
+public bool $showHeroModal;
+
+public $image;
+
+
 
     protected function rules(): array
     {
@@ -102,17 +107,8 @@ class LandingpageHero extends Component
     {
         $this->authorize("save", $this->hero);
         $this->validate();
-        $this->assignHeroProperties();
-        $this->handleImageUpload();
-        $this->hero->save();
-        $this->showHeroModal = false;
-        $this->dispatch("notify", "Saved!");
-    }
-
-    private function assignHeroProperties(): void
-    {
         $user = auth()->user();
-        $this->hero->user()->associate($user);
+        $this->hero->user_id = $user->id;
         $this->hero->mainQuote = $this->mainQuote;
         $this->hero->secondaryQuote = $this->secondaryQuote;
         $this->hero->thirdQuote = $this->thirdQuote;
@@ -122,10 +118,8 @@ class LandingpageHero extends Component
         $this->hero->gradientSecondColor = $this->gradientSecondColor;
         $this->hero->gradientDegreeEnd = $this->gradientDegreeEnd;
         $this->hero->waves = $this->waves;
-    }
 
-    private function handleImageUpload(): void
-    {
+        $newImageUploaded = false;
         if ($this->image instanceof UploadedFile) {
             // Delete the old image file
             Storage::disk('s3-public')->delete($this->hero->image);
@@ -133,54 +127,18 @@ class LandingpageHero extends Component
             // Save the new image file
             $filename = $this->image->store("heroImage", "s3-public");
             $this->hero->image = $filename;
-        } elseif ($this->hero->getOriginal("image")) {
+
+            $newImageUploaded = true;
+        }
+
+        if (!$newImageUploaded && $this->hero->getOriginal("image")) {
             // If no new image was uploaded and the original model has an image, retain the original image
             $this->hero->image = $this->hero->getOriginal("image");
         }
+        $this->hero->save();
+        $this->showHeroModal = false;
+        $this->dispatch("notify", "Saved!");
     }
-
-//    #[On('saved')]
-//    public function save(): void
-//    {
-//        $this->commitSave();
-//    }
-//
-//    protected function commitSave(): void
-//    {
-//        $this->authorize("save", $this->hero);
-//        $this->validate();
-//        $user = auth()->user();
-//        $this->hero->user_id = $user->id;
-//        $this->hero->mainQuote = $this->mainQuote;
-//        $this->hero->secondaryQuote = $this->secondaryQuote;
-//        $this->hero->thirdQuote = $this->thirdQuote;
-//        $this->hero->gradientDegree = $this->gradientDegree;
-//        $this->hero->gradientFirstColor = $this->gradientFirstColor;
-//        $this->hero->gradientDegreeStart = $this->gradientDegreeStart;
-//        $this->hero->gradientSecondColor = $this->gradientSecondColor;
-//        $this->hero->gradientDegreeEnd = $this->gradientDegreeEnd;
-//        $this->hero->waves = $this->waves;
-//
-//        $newImageUploaded = false;
-//        if ($this->image instanceof UploadedFile) {
-//            // Delete the old image file
-//            Storage::disk('s3-public')->delete($this->hero->image);
-//
-//            // Save the new image file
-//            $filename = $this->image->store("heroImage", "s3-public");
-//            $this->hero->image = $filename;
-//
-//            $newImageUploaded = true;
-//        }
-//
-//        if (!$newImageUploaded && $this->hero->getOriginal("image")) {
-//            // If no new image was uploaded and the original model has an image, retain the original image
-//            $this->hero->image = $this->hero->getOriginal("image");
-//        }
-//        $this->hero->save();
-//        $this->showHeroModal = false;
-//        $this->dispatch("notify", "Saved!");
-//    }
 
     public function render(): Factory|View|Application
     {
